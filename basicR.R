34f1dc17f49f03013be4931528ba9d18deb4a1c4
Carlos Henrique <henriqueetec1987@gmail.com>
2024-12-06 20:24:10 -0300

    commit initial

---
+++ basicR.R
@@ -0,0 +1,30 @@
+# ? help about that type
+?print
+# variables
+first_variable <- "This my variable"
+second_variable <- 12.5
+
+# vector
+vec_1 <- c(10.0,9.5,12.8)
+x <- vec_1
+is.double(x)
+length(x)
+?median
+median(vec_1)
+median(c(10.0,9.5,1))
+var_t <- c(1,2,3)
+names(var_t) <- c('a','b','c')
+var_t
+
+# List
+l <- list('Carlos', 37L, 'Brazilian', is.logical(TRUE))
+names(l) <- list("Name ", "Age", "Country")
+l
+
+#date
+today()
+now()
+
+#Data frames
+print(top_ten_teams)
+print(top_ten_teams)
