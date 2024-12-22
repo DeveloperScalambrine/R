8f8d91a9f76ded29baf4395d63eb1a2604f568c4
Carlos Henrique <henriqueetec1987@gmail.com>
2024-12-22 00:12:34 -0300

    done examples using news functions

--- Cleaning-data/example_1.R
+++ Cleaning-data/example_1.R
@@ -1,1 +1,18 @@
-# somente para subir para o git teste
+skim_without_charts(penguins)
+glimpse(penguins)
+data_na <- penguins %>%
+  select(species, sex)
+
+data_resume <- na.omit(penguins) %>%
+  group_by(species, sex) %>%
+  summarise(quantidade = n(), .groups = 'drop')
+
+sum_for_specie <- data_resume %>%
+  group_by(species) %>%
+  summarise(total_por_especie = sum(quantidade), .groups = 'drop')
+
+print("Contagem por sexo:")
+print(data_resume)
+
+print("Soma total por espécie:")
+print(sum_for_specie)
