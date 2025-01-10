1ccefca23535e44d03fd12df6b99f69830a4ba19
Carlos Henrique <henriqueetec1987@gmail.com>
2025-01-10 02:21:58 -0300

    add last exercises

---
+++ preview/exercise-graph.R
@@ -0,0 +1,28 @@
+hotel_bookings <- read_csv("R/preview/hotel.csv")
+
+ggplot(data=hotel_bookings) +
+  geom_bar(mapping = aes(x=distribution_channel, fill = deposit_type))
+
+ggplot(data=hotel_bookings) +
+  geom_bar(mapping = aes(x=distribution_channel, fill = market_segment))
+
+ggplot(data = hotel_bookings) +
+  geom_bar(mapping = aes(x=distribution_channel)) +
+  facet_wrap(~deposit_type) +
+  theme(axis.text.x = element_text(angle = 45))
+
+
+ggplot(data = hotel_bookings) +
+  geom_bar(mapping = aes(x=distribution_channel)) +
+  facet_wrap(~market_segment) +
+  theme(axis.text.x = element_text(angle = 45))
+
+ggplot(data = hotel_bookings) +
+  geom_bar(mapping = aes(x = distribution_channel)) +
+  facet_grid(~deposit_type) +
+  theme(axis.text.x = element_text(angle = 45))
+
+ggplot(data = hotel_bookings) +
+  geom_bar(mapping = aes(x = distribution_channel)) +
+  facet_wrap(~deposit_type~market_segment) +
+  theme(axis.text.x = element_text(angle = 45))
