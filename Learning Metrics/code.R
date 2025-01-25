b325d6f30e78b0366e24bbd1ba2e9e4cb5cc5339
CARLOS HENRIQUE SCALAMBRINE DE SOUZA <135667372+DeveloperScalambrine@users.noreply.github.com>
2025-01-25 02:43:56 -0300

    Add code and done task

--- Learning Metrics/code.R
+++ Learning Metrics/code.R
@@ -1,1 +1,15 @@
+y_true <- c('dog', 'cat', 'cat', 'cat', 'dog', 'dog')  # Value True
+y_pred <- c('dog','cat','cat','cat','dog','dog')  # Value predicted
+
+confusion_matrix <- confusionMatrix(factor(y_pred), factor(y_true))
+# values of matrix confusion
+confusion_matrix
+
+# using a library ggplot for visualization of results
+ggplot(as.data.frame(confusion_matrix$table), aes(Prediction, Reference))
+    geom_tile(aes(fill = Freq))
+    geom_text(aes(label = Freq))
+    scale_fill_gradient(low = "white", high = "steelblue")
+    theme_bw() 
+    ggtitle("Matrix of Confusion")
 
